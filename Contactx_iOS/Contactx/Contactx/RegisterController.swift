c5935139c91bc36e4b1a6b43d4207306c9e13385
michael <dr.mike.attia@gmail.com>
2017-04-29 23:38:20 +0200

    RESTful Jersey API \n iOS Storyboard

---
+++ Contactx_iOS/Contactx/Contactx/RegisterController.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterController.swift
+//  Contactx
+//
+//  Created by Michael on 4/29/17.
+//  Copyright © 2017 MAD. All rights reserved.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func returnToSignIn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
